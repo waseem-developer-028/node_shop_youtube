1044e4a4cef3f1bbe93709b15dc00c8b3bd2aa1d
waseem <waseemtcl028@gmail.com>
2025-08-18 00:33:55 +0530

    fix: update Gmail SMTP configuration chpt

--- heplers/sendemail.js
+++ heplers/sendemail.js
@@ -1,72 +1,72 @@
-const nodeMailer = require('nodemailer')
-const { renderFile } = require("ejs")
-const path = require('path')
+const nodeMailer = require("nodemailer");
+const { renderFile } = require("ejs");
+const path = require("path");
 
 // Get the root directory for the application
-const appRootDir = path.resolve(process.cwd())
+const appRootDir = path.resolve(process.cwd());
 
-const sendMail      = async (to,subject,text, sendData={test:'test'},attachment=null) =>{
- 
-try{    
-var transporter = nodeMailer.createTransport({
-        host:process.env.EMAIL_HOST,
-        port:process.env.EMAIL_PORT,
-        secure:process.env.SECURE,
-        // requireTls:true,
-        //cc:"test@gmail.com",
-        //bcc:"test@gmail.com",
-        auth:{
-            user:process.env.EMAIL_EMAIL,
-            pass:process.env.EMAIL_PASS
-        }
-  });
+const sendMail = async (
+  to,
+  subject,
+  text,
+  sendData = { test: "test" },
+  attachment = null
+) => {
+  try {
+    var transporter = nodeMailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: process.env.SECURE,
+      // requireTls:true,
+      //cc:"test@gmail.com",
+      //bcc:"test@gmail.com",
+      auth: {
+        user: process.env.EMAIL_EMAIL,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-       let htmlText = null
-       try{
-       htmlText = await renderFile(appRootDir + "/views/mail/"+text, sendData)
-       }
-       catch(e){
-         helper.myConsole("Email File Ejs Error="+e);
-         return e
-       }
-    
-  
-  var mailOptions = {
-    from: process.env.EMAIL_FROM_EMAIL,
-    to:to,
-    subject:subject,
-    text:'',
-    html:htmlText,
-  };
+    let htmlText = null;
+    try {
+     // htmlText = await renderFile(appRootDir + "/views/mail/" + text, sendData);
+      htmlText = await renderFile(path.join(appRootDir, "views", "mail", text), sendData)
+    } catch (e) {
+      helper.myConsole("Email File Ejs Error=" + e);
+      return e;
+    }
 
-     if(attachment!=null)
-     {
-        
-      mailOptions= {...mailOptions,
-           attachments: [{
-           filename: attachment.filename,
-           path: attachment.filepath,
-           contentType:  attachment.contentType
-          }]
-        }
-     }
+    var mailOptions = {
+      from: process.env.EMAIL_FROM_EMAIL,
+      to: to,
+      subject: subject,
+      text: "",
+      html: htmlText,
+    };
 
- 
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } 
-  });
+    if (attachment != null) {
+      mailOptions = {
+        ...mailOptions,
+        attachments: [
+          {
+            filename: attachment.filename,
+            path: attachment.filepath,
+            contentType: attachment.contentType,
+          },
+        ],
+      };
+    }
 
-}
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  } catch (e) {
+    console.log("Send Email Error=" + e);
+    return e;
+  }
 
-catch(e){
-    console.log("Send Email Error="+e);
-    return e
-}
+  return true;
+};
 
-  return true
-
-}
-
-module.exports = sendMail
+module.exports = sendMail;
