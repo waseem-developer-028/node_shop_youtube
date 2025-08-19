ffcae63f8af5b25da33424e241b6288572171d6a
waseem <waseemtcl028@gmail.com>
2025-08-19 13:55:42 +0530

    fix: server error fix

--- routes/v1/invoice/index.js
+++ routes/v1/invoice/index.js
@@ -2,11 +2,10 @@ const { Router } = require("express");
 const { authenticate } = require("../../../middlewares/auth");
 const {
   index,
-  orderInvoice,
 } = require("../../../controllers/invoice/invoice.controller");
 const router = Router();
 
 router.get("/orderinvoice", index);
-router.get("/orderinvoiceinfo", authenticate, orderInvoice);
+
 
 module.exports = router;
